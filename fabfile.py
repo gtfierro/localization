761f41a1b4bd6fecabb9634c6a48a8987671c156
root <root@fitpc09.(none)>
2012-07-24 15:53:35 -0700

    Additional features for fabric

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import *
+
+env.user = 'root'
+env.hosts = ['128.32.156.45', '128.32.156.64', '128.32.156.67', '128.32.156.131']
+env.warn_only = True
+
+@parallel
+def uname():
+  run('uname',shell=False)
+
+@parallel
+def kill_tcpdump():
+  run('/usr/bin/killall -9 tcpdump',shell=False)
+
+@parallel
+def set_monitor():
+  run('/usr/sbin/iw dev wlan0 del',shell=False)
+  run('/usr/sbin/iw phy phy0 interface add wlan0 type monitor',shell=False)
+  run('/usr/sbin/iw dev wlan0 set txpower fixed 0',shell=False)
+  run('/sbin/ifconfig wlan0 up',shell=False)
+
+@parallel
+def set_channel(nic='nic',chan='chan'):
+  chan=int(chan)
+  run('/usr/sbin/iw dev %s set channel %d' % (nic, chan), shell=False)
