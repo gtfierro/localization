113af00744965d29b649d2e8fe55d9c4c8c9f09d
Gabe Fierro <gtfierro225@gmail.com>
2012-07-30 14:27:35 -0700

    adding graphics support

--- occupancy/router/driver.py
+++ occupancy/router/driver.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import numpy
 import time
 import json
+import sys
 
 class Localizer:
     def __init__(self, channels = [1,6,11]):
@@ -71,16 +72,10 @@ class Localizer:
         for mac in all_macs:
             owner = max( [(collector_macs[c.server][mac], c.server)  for c in self.collectors] , key=lambda x: x[0])[1]
             owned_macs[owner].append(mac)
-        for c in self.collectors:
-          print c.server
-          print len(owned_macs[c.server])
-          print
-        owned_macs = collector_macs = all_macs = None
-        print
-        print
+        return owned_macs
 
 
-    def run(self):
+    def run(self,graphics=False):
         for c in self.collectors:
             c.start_channel_cycle()
         for c in self.collectors:
@@ -91,17 +86,30 @@ class Localizer:
         sample_period = 3
         # No data for last no_data_count seconds 
         no_data_count = 0
+        if graphics:
+            import matplotlib
+            matplotlib.use('TkAgg')
+            import matplotlib.pyplot as plt
+            plt.ion()
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
 
+        timestamp = 0
         while True:
+
             # Sample for n seconds
             self.mgr.poll(sample_period)
-            #(medians, counts) = self._median_signals()
-            print self._get_occupancy()
-            #zone = medians.index(max(medians))+2
-
-            #print zone, avgs
-            #print zone, medians, sum(counts)
+            owned_macs = self._get_occupancy()
+            if graphics:
+              timestamp += .1
+              for (s,style) in zip(owned_macs,['r.','b.','g.','c.']):
+                  ax.plot(timestamp, len(owned_macs[s]), style)
+              plt.draw()
+            else:
+              print [(c, len(owned_macs[c])) for c in owned_macs]
 
 if __name__ == '__main__':
     l = Localizer()
-    l.run()
+    if len(sys.argv) > 1:
+      graphics = int(sys.argv[1])
+    l.run(graphics)
