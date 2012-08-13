d55ad51e4e60086293891620afd5802e37dfae6c
Gabe Fierro <gtfierro225@gmail.com>
2012-08-13 12:31:39 -0700

    Popen for cycle commands, adding graphics support
    Use Popen so we don't have to use nohup to detach fro mthe process

--- occupancy/router/pipe.py
+++ occupancy/router/pipe.py
@@ -5,6 +5,7 @@ import subprocess
 import shlex
 import numpy
 import pickle
+import time
 from collections import defaultdict
 from pprint import pprint
 from prun import CmdRun, IOMgr
@@ -24,6 +25,7 @@ class Collector:
         self.routers = {}   #key = router-ip, value = router tcpdump proc
         self.monitors = {}  #key = router-ip, value = monitor proc
         self.macs = {}      #key = router-ip, value = dict of mac addresses to list of detected signals
+        self.cycles = {}    #key = router-ip, value = router cycle proc
 
         self.count = 0
         self.mgr = mgr
@@ -37,14 +39,14 @@ class Collector:
                 '/usr/sbin/iw dev wlan0 set txpower fixed 0',
                 '/usr/sbin/iw dev wlan0 set channel 1',
                 '/sbin/ifconfig wlan0 up',
-                '/bin/sh cycle.sh &']
+                '/bin/sh cycle.sh']
 
         print "Setting up routers..."
         #setup monitoring on routers
         for router in list(router_list):
-            cmd_string = 'ssh root@%s "%s"' % (router, ';'.join(cmds))
-            print '  ',cmd_string
-            subprocess.call(cmd_string,bufsize=1,shell=True)
+            cmd = 'ssh root@%s "%s"' % (router, ';'.join(cmds))
+            print '  ',cmd
+            self.cycles[router] = subprocess.Popen(cmd,shell=True)
             subprocess.call('rm /tmp/%s ; mkfifo /tmp/%s' % (router,router), shell=True)
             print '  ',router,'done!'
 
@@ -75,12 +77,13 @@ class Collector:
             self.monitors[m].kill()
         for r in self.routers:
             self.routers[r].kill()
+        for c in self.cycles:
+            self.cycles[c].kill()
 
     def clear_data(self):
         """
         Re-initializes internal data structures for records
         """
-        self.power = []
         for r in self.macs:
             self.macs[r] = defaultdict(list)
         self.count = 0
@@ -88,12 +91,12 @@ class Collector:
 
     def get_data(self):
         """
-        Returns self.macs, but only mac addressese with at least one third of the sample size packets
-        will be returned. For those that are returned, the median signal is returned
+        Returns self.macs, but only mac addressese with at least one third of the sample size packets OR at 
+        least 20 packets (the max of these) will be returned. For those that are returned, the median signal is returned
         """
         for router in self.macs:
             for mac in self.macs[router]:
-                if len(self.macs[router][mac]) >= (.33 * self.sample_period):
+                if len(self.macs[router][mac]) >= max(20, .33 * self.sample_period):
                     self.macs[router][mac] = numpy.median(self.macs[router][mac])
                 else:
                     self.macs[router][mac] = None
@@ -115,13 +118,28 @@ class Collector:
             self.macs[ip][mac].append(int(db))
             self.count += 1
 
-def main(sample_period):
+def main(sample_period, graphics=False):
     mgr = IOMgr()
-    c = Collector(mgr,sample_period,"128.32.156.64","128.32.156.67","128.32.156.131")
+    c = Collector(mgr,sample_period,"128.32.156.64","128.32.156.67","128.32.156.131","128.32.156.45")
+    if graphics:
+        import matplotlib
+        matplotlib.use('TkAgg')
+        import matplotlib.pyplot as plt
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        timestamp = 0
     while True:
         try:
+            time.sleep(sample_period)
             mgr.poll(sample_period)
-            print c.get_data()
+            data = c.get_data()
+            if graphics:
+                timestamp += sample_period
+                for (router, style) in zip(data, ['r.','b.','g.','k.']):
+                    ax.plot(timestamp, len(data[router]), style)
+                plt.draw()
+            print [(router, len(data[router])) for router in data]
             pickle.dump(c.macs,open('macs.db','wb'))
             c.clear_data()
         except KeyboardInterrupt:
@@ -129,4 +147,4 @@ def main(sample_period):
             sys.exit(0)
 
 if __name__=="__main__":
-    main(int(sys.argv[1]) if len(sys.argv) > 1 else 10)
+    main(int(sys.argv[1]) if len(sys.argv) > 1 else 10, int(sys.argv[2]) if len(sys.argv) > 2 else 0)
