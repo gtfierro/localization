cabdb1dce108e1ffe8e7d7eccbbca696dc89595b
Gabe Fierro <gtfierro225@gmail.com>
2012-08-13 14:46:15 -0700

    using .5 second intervals for cycling

--- cycle.sh
+++ cycle.sh
@@ -1,1 +1,1 @@
-while [ 1 ] ; do sleep 1 ; /usr/sbin/iw wlan0 set channel 1 ; sleep 1 ; /usr/sbin/iw wlan0 set channel 6 ; sleep 1 ; /usr/sbin/iw wlan0 set channel 11 ; done
+while [ 1 ] ; do perl -e "select(undef, undef, undef, 0.5);" ; /usr/sbin/iw wlan0 set channel 1 ; perl -e "select(undef, undef, undef, 0.5);" ; /usr/sbin/iw wlan0 set channel 6 ; perl -e "select(undef, undef, undef, 0.5);" ; /usr/sbin/iw wlan0 set channel 11 ; done
