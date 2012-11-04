3c5160317ba8ade2cbf1de15145a2f10ef2f328c
Andrew Krioukov <krioukov@gmail.com>
2012-11-04 15:55:22 -0800

    Add user prefs

--- mapdemo/app.py
+++ mapdemo/app.py
@@ -1,22 +1,63 @@
 from flask import Flask, request, jsonify, Response, render_template
+import json
+import redis
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('base.html')
+    return render_template('base.html', user_ip = request.remote_addr)
 
-@app.route("/android")
-def test():
-    return render_template('and_base.html')
-
-@app.route("/data")
-def getdata():
+@app.route("/zone_data")
+def zone_data():
     try:
         f = open('../occupancy/router/data.json', 'r')
         return Response(f.read(), mimetype='application/json')
     except:
         return jsonify(error=1)
+
+def _get_default_pref():
+  h = random.randint(0, 360)
+  s = random.random()
+  l = random.random()
+  return {'h': h, 's': s, 'l': l}
+
+@app.route("/client_data")
+def client_data():
+    r = redis.Redis()
+    data = []
+    loc_dict = r.hgetall('client_location')
+    for (mac, loc_data) in loc_dict.items():
+      pref_data = r.hget('client_pref', mac)
+      if pref_data == None:
+        pref = _get_default_pref()
+        r.hset('client_pref', mac, json.dumps(pref))
+      else:
+        pref = json.loads(pref_data)
+
+      d = json.loads(loc_data)
+      client_data = {'mac': mac, 'x': d[0], 'y': d[1], 'ip': d[2]}
+      client_data.update(pref)
+      data.append(client_data)
+
+    return jsonify(data=data)
+
+@app.route('/update_pref')
+def update_pref():
+    ip = request.remote_addr
+    h = request.args.get('h')
+    s = request.args.get('s')
+    l = request.args.get('l')
+
+    data = json.dumps({'h': h, 's': s, 'l': l})
+    r = redis.Redis()
+    mac = r.hget('ipmac', ip)
+    if mac == None:
+      return jsonify(error="Unknown IP")
+
+    r.hset('client_pref', mac, data)
+    return jsonify(success=True)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
---
+++ mapdemo/fake_localization.py
@@ -0,0 +1,17 @@
+import redis
+import time
+import json
+import random
+
+r = redis.Redis()
+
+while True:
+  for (mac, ip) in zip(['0a:f6:b1:19:aa:bb', '0a:f6:b1:19:00:11', '0a:f6:b1:19:cc:dd'],
+                     ['127.0.0.1', '10.10.123.123', '10.10.123.124']):
+    r.hset('macip', mac, ip)
+    r.hset('ipmac', ip, mac)
+
+    data = json.dumps([random.randint(0,599), random.randint(0, 240), ip])
+    print mac, data
+    r.hset('client_location', mac, data)
+  time.sleep(3)
