a7342deb93abaf4011b0e61ddc9ac8409b857557
Gabe Fierro <gtfierro225@gmail.com>
2012-11-04 19:06:35 -0800

    adding new pinger

--- occupancy/router/pinger.py
+++ occupancy/router/pinger.py
@@ -1,9 +1,19 @@
 import redis
 import os
+import subprocess
 
 r = redis.Redis()
 
-while True:
-    for ip in r.hgetall('ipmac'):
-        os.system('ping -c 1 %s' % ip)
+processes = {} # key: client-ip, value: ping process
+
+try:
+  while True:
+      for ip in r.hgetall('ipmac'):
+          if ip not in processes.keys():
+              processes[ip] = subprocess.Popen('ping %s' % ip, shell=True)
+except Exception as e:
+  print e
+  for ip in processes:
+      processes[ip].kill()
+
     
