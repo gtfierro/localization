cca0c3a4efb998d77f15de6d94bc5be7bd78b8dc
Andrew Krioukov <krioukov@gmail.com>
2012-11-04 20:37:51 -0800

    Add pinger

---
+++ mapdemo/static/assets/js/ping.js
@@ -0,0 +1,15 @@
+function make_req() {
+  var d = new Date();
+  $.ajax(
+    'url': '/dummy',
+    'data': {'d': d.getTime()},
+    'timeout': 100,
+    'complete': function() {
+      console.log('complete');
+      make_req();
+    }
+  );
+}
+$(function() {
+  make_req();
+});
