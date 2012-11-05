6d0717f008c7916ea8265d986cf1af51de773bab
Andrew Krioukov <krioukov@gmail.com>
2012-11-04 20:42:00 -0800

    fix pinger

--- mapdemo/static/assets/js/ping.js
+++ mapdemo/static/assets/js/ping.js
@@ -1,14 +1,14 @@
 function make_req() {
   var d = new Date();
-  $.ajax(
+  $.ajax({
     'url': '/dummy',
     'data': {'d': d.getTime()},
     'timeout': 100,
     'complete': function() {
       console.log('complete');
-      make_req();
+      setTimeout("make_req();", 250);
     }
-  );
+  });
 }
 $(function() {
   make_req();
